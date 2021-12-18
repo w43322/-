05c192cdd676daea220639e88eeddf49e5bb0e0b
w43322 <877961980@qq.com>
2021-12-19 05:30:46 +0800

    final update

--- 实验/4/avltree.h
+++ 实验/4/avltree.h
@@ -1,10 +1,8 @@
 #ifndef AVLTREE_H
 #define AVLTREE_H
 
-#endif // AVLTREE_H
-
-#include <QLabel>
-#include <QDebug>
+#include <qlabel.h>
+#include <qdebug.h>
 
 struct AVLNode
 {
@@ -13,23 +11,16 @@ struct AVLNode
     QLabel lab,Llink,Rlink;
 };
 
-class AVL
-{
-private:
+int calheight(struct AVLNode *);
+int bf(struct AVLNode *);
+struct AVLNode *llrotation(struct AVLNode*);
+struct AVLNode *lrrotation(struct AVLNode*);
+struct AVLNode *rlrotation(struct AVLNode*);
+struct AVLNode *rrrotation(struct AVLNode*);
+struct AVLNode *insert(struct AVLNode*,int);
+struct AVLNode *deleteAVLNode(struct AVLNode*,int,struct AVLNode*);
+struct AVLNode *inpre(struct AVLNode*);
+struct AVLNode *insuc(struct AVLNode*);
+struct AVLNode *FIND(struct AVLNode*,int);
 
-public:
-    struct AVLNode * root;
-    AVL();
-    ~AVL();
-    int calheight(struct AVLNode *);
-    int bf(struct AVLNode *);
-    struct AVLNode * llrotation(struct AVLNode *);
-    struct AVLNode * lrrotation(struct AVLNode *);
-    struct AVLNode * rlrotation(struct AVLNode *);
-    struct AVLNode * rrrotation(struct AVLNode *);
-    struct AVLNode* insert(struct AVLNode *,int);
-    struct AVLNode* deleteAVLNode(struct AVLNode *,int);
-    struct AVLNode* inpre(struct AVLNode*);
-    struct AVLNode* insuc(struct AVLNode*);
-    struct AVLNode* find(struct AVLNode*,int);
-};
+#endif // AVLTREE_H
